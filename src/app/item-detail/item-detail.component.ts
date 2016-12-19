6b90bfc15cfea2694524dadce1b76d8929594ab4
RobRoch <rochon18@gmail.com>
2016-12-19 20:30:12 +0100

    back button works in item detail

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import { ItemsService } from '../shared/model/items.service';
 import { Item } from '../shared/model/item';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-item-detail',
@@ -16,7 +17,8 @@ export class ItemDetailComponent implements OnInit {
 
   constructor( private route: ActivatedRoute,
                private router: Router,
-               private itemsService: ItemsService) { }
+               private itemsService: ItemsService,
+               private location: Location) { }
 
   ngOnInit() {
 
@@ -25,6 +27,10 @@ export class ItemDetailComponent implements OnInit {
             return this.itemsService.findItemByKey($key);
         })
         .subscribe(item => this.item = item);
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
